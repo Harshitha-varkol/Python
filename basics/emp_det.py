2fc17730365523a0b46133dedcb56ab815b70299
lovely <lillyflow729@gmail.com>
2025-08-22 23:59:47 +0530

    Added basics folder with three files

--- basics/emp_det.py
+++ basics/emp_det.py
@@ -1,16 +1,1 @@
-emp_name="AKSHITHA"
-print(id(emp_name))
-print(type(emp_name))
-emp_id=12
-print(id(emp_id))
-print(type(emp_id))
-emp_email="akshithakane1209@gmail.com"
-print(id(emp_email))
-print(type(emp_email))
-emp_mobile_no=9121456789
-print(id(emp_mobile_no))
-print(type(emp_mobile_no))
-emp_salary=600000
-print(id(emp_salary))
-print(type(emp_salary))
-
+ 
