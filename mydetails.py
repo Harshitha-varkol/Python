fcff54cb5cf3e30705e9c6a1d948cbbfcbf159d9
Harshitha-varkol <lillyflow729@gmail.com>
2025-08-22 23:55:43 +0530

    Add files via upload
    Basics of python

---
+++ mydetails.py
@@ -0,0 +1,24 @@
+
+print("MY DETAILS")
+print("NAME : AKSHITHA")
+print("DOB : 12/09/2003")
+print("MOBILE NO. : 9121867685")
+print("EMAIL : akshithavangari1@gmail.com")
+print("\n")
+
+print("LAPTOP DETAILS")
+print("DEVICE NAME : Desktop- NRAA2E1")
+print("RAM : 8.00 GB")
+print("SYSTEM TYPE : 64-bit operating system, x64-based processor")
+print("EDITION : Windows 10 Pro")
+print("\n")
+
+str="string"
+print(id(str))
+print(type(str))
+a1=1234567891011121314151617189
+print(id(a1))
+print(type(a1))
+a2=125.500
+print(id(a2))
+print(type(a2))
